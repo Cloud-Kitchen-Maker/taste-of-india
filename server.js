9d8305fcda7b2210d42814d953269383983cbfe3
Mustaque Ali <mustaq.md261984@gmail.com>
2024-11-29 09:17:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const transporter = nodemailer.createTransport({
 // Endpoint to handle orders
 app.post("/place-order", (req, res) => {
   const { orderDetails } = req.body;
-  const emailRecipients = ["9731340538@gmail.com", "9731340860@gmail.com"]; // Replace with real email addresses
+  const emailRecipients = ["mustaq.md261984@gmail.com", "salmathw.21@gmail.com"]; // Replace with real email addresses
 
   emailRecipients.forEach((recipient) => {
     const mailOptions = {
